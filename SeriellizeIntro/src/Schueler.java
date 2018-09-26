9568ec37033026d869978eedd299c45e7fb34447
Hang AmI <gradoc15@new.edvhtl.at>
2018-09-26 12:48:13 +0200

    added save

--- SeriellizeIntro/src/Schueler.java
+++ SeriellizeIntro/src/Schueler.java
@@ -1,3 +1,6 @@
+
+import java.time.LocalDate;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,5 +13,46 @@
  */
 public class Schueler
 {
+
+    public Schueler(String name, LocalDate bday)
+    {
+        this.name = name;
+        this.bday = bday;
+        this.age = LocalDate.now().getYear() - bday.getYear();
+    }
+    private String name;
+    private int age;
+    private LocalDate bday;
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public int getAge()
+    {
+        return age;
+    }
+
+    public void setAge(int age)
+    {
+        this.age = age;
+    }
+
+    public LocalDate getBday()
+    {
+        return bday;
+    }
+
+    public void setBday(LocalDate bday)
+    {
+        this.bday = bday;
+    }
+    
     
 }
--- SeriellizeIntro/src/SchuelerBl.java
+++ SeriellizeIntro/src/SchuelerBl.java
@@ -1,3 +1,7 @@
+
+import java.io.File;
+import java.util.ArrayList;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,5 +14,25 @@
  */
 public class SchuelerBl
 {
+    private ArrayList<Schueler> klasse = new ArrayList();
     
+    public void add(Schueler s)
+    {
+        klasse.add(s);
+    }
+    
+    public void save(File f)
+    {
+        
+    }
+    
+    public void load(File f)
+    {
+        
+    }
+    
+    public static void main(String[] args)
+    {
+        
+    }
 }
